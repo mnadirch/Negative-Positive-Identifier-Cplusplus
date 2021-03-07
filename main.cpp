aa5178c241b61e23a71145fa64b8c771ea7030c5
Mohammad Nadir <78723289+mnadirch@users.noreply.github.com>
2021-03-07 16:42:22 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,62 @@
+  #include<iostream>
+  
+  	using namespace std;
+ 
+ 		 int main()
+								
+	
+	  {
+  			int var1,var2,var3,var4,var5,sum;
+  
+  								cout<<"Enter 1st value=";
+  					 			cin>>var1;
+  								cout<<"Enter 2nd value=";
+   								cin>>var2;
+  								cout<<"Enter 3rd value=";
+  								cin>>var3;
+  								cout<<"Enter 4th value=";
+   								cin>>var4;
+  								cout<<"Enter 5th value=";
+  					 			cin>>var5;
+  					 			sum=var1+var2+var3+var4+var5;
+  					 			cout<<"The Sum is : "<<sum<<endl;
+  					 			 if(sum>50)
+  					 			 {
+  					 			 	cout<<"Greater than 50"<<endl;
+									}
+								 else if(sum<50)
+  					 			 {
+  					 			 	cout<<"Less than 50"<<endl;
+									}
+						         else
+  					 			 {
+  					 			 	cout<<"Equal to 50"<<endl;
+									}
+
+ 									  if(var1>=0)
+ 										 {cout<<var1<<" is positive."<<endl;}
+ 										else
+  										{cout<<" Negative Number "<<endl;}
+ 												 
+									 if(var2>=0)
+  											{cout<<var2<<" is positive."<<endl;}
+  										else
+  										{cout<<var2<<" Negative Number "<<endl;}
+
+  									 if(var3=0)
+ 											 {cout<<var3<<" is positive."<<endl;}
+ 											else
+  											{cout<<var3<<" Negative Number"<<endl;}
+  																					
+ 										 if(var4>=0)
+  											{cout<<var4<<" is positive."<<endl;}
+  											else
+  											{cout<<var4<<" Negative Number "<<endl;}
+  																				
+  										 if(var5>=0)
+  											{cout<<var5<<" is positive."<<endl;}
+  											else
+  											{cout<<var5<<" Negative Number"<<endl;}
+  										
+										  	return 0;
+																			  }
